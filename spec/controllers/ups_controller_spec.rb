2f7866135b89d56ff2b476269143a0320eb08bc3
Bri Dotson <bridotson@gmail.com>
2014-12-17 10:42:56 -0800

    add test for ups rates

--- spec/controllers/ups_controller_spec.rb
+++ spec/controllers/ups_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe UpsController, :type => :controller do
+  describe "rates" do
+    it 'returns a json object with shipping rates in cents' do
+      get :rates,:params => {
+        :weight => "100",
+        :country_o => 'US',
+        :state_o => 'CA',
+        :city_o => 'Beverly Hills',
+        :zip_o => '90210',
+        :country => 'CA',
+        :province => 'ON',
+        :city => 'Ottawa',
+        :postal_code => 'K1P 1J1'
+      }
 
+      expect(response)
+
+    end
+  end
 end
