1d098d34fa58cd1b90e89ebf682974685c99b4b1
Bri Dotson <bridotson@gmail.com>
2014-12-17 10:40:52 -0800

    generate ups controller now so rspec file will appear

--- app/controllers/ups_controller.rb
+++ app/controllers/ups_controller.rb
@@ -1,5 +1,2 @@
 class UpsController < ApplicationController
-  def rates
-    raise params.inspect
-  end
 end
---
+++ spec/controllers/ups_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe UpsController, :type => :controller do
+
+end
