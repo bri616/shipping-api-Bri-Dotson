5ea071f083421065d15c8c531cbaa53a6786ce6c
Bri Dotson <bridotson@gmail.com>
2014-12-17 13:10:26 -0800

    refactor for nested hashes in api query

--- app/controllers/ups_controller.rb
+++ app/controllers/ups_controller.rb
@@ -1,25 +1,19 @@
 class UpsController < ApplicationController
   def rates
-    specs = params[:specs]
+    package_specs = params[:package_specs]
+    origin_specs = params[:origin_specs]
+    destination_specs = params[:destination_specs]
 
-    if specs.blank?
-      render json: {error: "Must provide specifications"}, status: :bad_request
+    if package_specs.blank?
+      render json: {error: "Must provide package specifications"}, status: :bad_request
     else
-      package = Package.new(specs[:weight].to_i, nil)
+      packages = package_specs[:weights].collect {|weight| Package.new(weight.to_i, nil)}
 
-      origin = Location.new(
-      :country => specs[:country_o],
-      :state => specs[:state_o],
-      :city => specs[:city_o],
-      :zip => specs[:zip_o])
+      origin = Location.new(origin_specs)
 
-      destination = Location.new(
-      :country => specs[:country],
-      :state => specs[:state],
-      :city => specs[:city],
-      :zip => specs[:zip])
+      destination = Location.new(destination_specs)
 
-      response = $ups.find_rates(origin, destination, package)
+      response = $ups.find_rates(origin, destination, packages)
 
       ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
 
--- spec/controllers/ups_controller_spec.rb
+++ spec/controllers/ups_controller_spec.rb
@@ -3,17 +3,24 @@ require 'rails_helper'
 RSpec.describe UpsController, :type => :controller do
   describe "rates" do
     it 'returns a json object with shipping rates' do
-      get :rates,:specs => {
-        :weight => "100",
-        :country_o => 'US',
-        :state_o => 'CA',
-        :city_o => 'Beverly Hills',
-        :zip_o => '90210',
-        :country => 'US',
-        :state => 'AZ',
-        :city => 'Chandler',
-        :zip => '85225'
-      }
+      get :rates,
+        :package_specs => {
+          :weights => ["100"]
+          },
+
+        :origin_specs => {
+          :country => 'US',
+          :state => 'CA',
+          :city => 'Beverly Hills',
+          :zip => '90210'
+          },
+          
+        :destination_specs => {
+          :country => 'US',
+          :state => 'AZ',
+          :city => 'Chandler',
+          :zip => '85225'
+        }
 
       expect(JSON.parse(response.body.to_s)["UPS Ground"]).to be > 0
 
